8136988ffc642cc0fc097d95a9e568d90e3f59ea
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-07-13 19:44:48 -0700

    updated contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,48 +1,99 @@
 import React from "react";
 // import ContactForm from "../../components/ContactForm/ContactForm";
-import { Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { GrGithub, GrLinkedin, GrPhone } from "react-icons/gr";
 import "./contact.css";
-
 
 function Contact() {
   return (
     <>
       <Row>
-        {/* Contact header text */}
+        {/* Contact text img */}
         <Col className="fs-2 fw-bolder">
           <Image
-            className=""
-            src={process.env.PUBLIC_URL + "/images/contact.png"}
+            className="mx-auto d-block"
+            src={process.env.PUBLIC_URL + "/images/contact-145766.png"}
             rounded
             fluid
           />
         </Col>
       </Row>
-      {/* Put contact form here */}
-     
-      <Row></Row>
+      {/* Put contact form here when finished */}
+      <Container className="">
+        <Row className="">
+          <h3 className="contacttext mt-4 mb-4 text-center">ways to find me:</h3>
+        </Row>
+        </Container>
 
-      <Row className="mt-5 mb-3 align-items-center" fluid>
-        <h3 className="contacttext mb-4">other ways to find me:</h3>
+        <Container className="justify-content-center">
+        <Row className="ml-5 mr-5 justify-content-md-center"><Col></Col><Col>
+        <Table className="table table-borderless" style={{height: "350px", }}>
+          <tbody className="ml-5 mr-5 ">
+            <tr >
+              {/* email */}
 
-        <Col className="contacttext fs-6 col-5" fluid>
-          <p className="align-items-center">
-            <GrPhone className="mobile fs-3"></GrPhone> 602-619-4644
-          </p>
-        </Col>
-        <Col className="fs-2" fluid>
-          <Link to="https://github.com/sarahbinaz1020">
-            <GrGithub></GrGithub>
-          </Link>
-        </Col>
-        <Col className="fs-2" fluid>
-          <Link to="https://www.linkedin.com/in/sarah-bonnell-1965555a/">
-            <GrLinkedin></GrLinkedin>
-          </Link>
-        </Col>
-      </Row>
+              <td className=""  >
+                <span style={{ color: "white" }} className="align-middle">
+                  {" "}
+                  <i className="far fa-envelope fa-3x "></i>
+                </span>
+              </td>
+              <td className="">
+                {" "}
+                <Link to="sarahbinaz1020@gmail.com">
+                  {" "}
+                  <p className="emailtext">sarahbinaz1020@gmail.com</p>
+                </Link>
+              </td>
+            
+           
+            </tr>
+            <tr className="">
+              {/* phone */}
+              
+              <td>
+                <span style={{ color: "white" }}>
+                  {" "}
+                  <i className="icon-phone fas fa-phone-alt fa-3x"></i>
+                </span>
+              </td>
+              <td>
+                {" "}
+                <p className="emailtext">602-619-4644 </p>
+              </td>
+            </tr>
+            <tr>
+              {/* github */}
+              
+              <td>
+                <span style={{ color: "white" }}>
+                  <i className="fab fa-github fa-3x"></i>
+                </span>
+              </td>
+              <td>
+                <Link to="https://github.com/sarahbinaz1020">
+                  <p className="emailtext">https://github.com/sarahbinaz1020</p>
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              {/* linkedin */}
+              <td>
+                <span style={{ color: "white" }}>
+                  <i className="fab fa-linkedin fa-3x"></i>
+                </span>
+              </td>
+              <td>
+                <Link to="https://www.linkedin.com/in/sarah-bonnell-1965555a/">
+                  <p className="emailtext">
+                    https://www.linkedin.com/in/sarah-bonnell-1965555a/
+                  </p>
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </Table></Col><Col></Col></Row>
+      </Container>
     </>
   );
 }
