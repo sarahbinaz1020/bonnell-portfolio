1fc95378b23af3917b5b71da2c308300c353d8af
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-07-13 20:37:10 -0700

    navbar fixes

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,25 +1,37 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import "./navbar.css"
-
+import "./navbar.css";
 
 function NavBar(props) {
   return (
     <>
-     <Navbar className="navbarbgd" expand="lg">
+      <Navbar className="navbarbgd" expand="lg">
         <figure className="figure">
-  <img src={process.env.PUBLIC_URL + "/images/S-logo-white-sm.png"} className="figure-img img-fluid rounded" alt="..."/>
-</figure>
+          <img
+            src={process.env.PUBLIC_URL + "/images/S-logo-white-sm.png"}
+            className="figure-img img-fluid rounded"
+            alt="..."
+          />
+        </figure>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse
+          className="justify-content-end mr-5"
+          id="basic-navbar-nav"
+        >
           <Nav className="ml-auto mt-5 mr-4 fs-4">
-            <Nav.Link className="text-light mr-4" href="#/">home</Nav.Link>
-            <Nav.Link  className="text-light mr-4" href="#/Portfolio">portfolio</Nav.Link>
-            <Nav.Link  className="text-light mr-4" href="#/Contact">contact</Nav.Link>
+            <Nav.Link className="text-light mr-4" href="#/">
+              home
+            </Nav.Link>
+            <Nav.Link className="text-light mr-4" href="#/Portfolio">
+              portfolio
+            </Nav.Link>
+            <Nav.Link className="text-light mr-4" href="#/Contact">
+              contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-  </>
+    </>
   );
 }
 
