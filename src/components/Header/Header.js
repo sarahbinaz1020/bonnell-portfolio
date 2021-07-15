65a1c9c8b22848842890186d1297ff2ff85d1281
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-07-15 10:49:13 -0700

    resume link fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,7 +34,7 @@ function Header() {
             </p>
             <p className="headertxt">Feel free to peruse; I welcome any feedback.</p>
             <Router basename="https://sarahbinaz1020.github.io/bonnell-portfolio/#/"/>
-<Link to={{ pathname: "https://drive.google.com/file/d/1PdPPVWDQrDSOSrAmNP79qaqPq1I1TRTT/view?usp=sharing" target: "_blank" }}>
+<Link to={{ pathname: "https://drive.google.com/file/d/1PdPPVWDQrDSOSrAmNP79qaqPq1I1TRTT/view?usp=sharing"}} target="_blank" >
             
             {/* <a href="https://drive.google.com/file/d/1PdPPVWDQrDSOSrAmNP79qaqPq1I1TRTT/view?usp=sharing" target="_blank" rel="noreferrer"> */}
             Click here to view my resume
