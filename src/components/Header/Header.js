13548f176564c96aec041144dbf06d56eacdf910
sarahbinaz1020 <sarahbinaz1020@gmail.com>
2021-07-20 20:33:26 -0700

    reworking of text on homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ function Header() {
         <Col>
           <Image
             className="mainImage mt-5"
-            src={process.env.PUBLIC_URL + "/images/run-with-chase.jpg"}
+            src={process.env.PUBLIC_URL + "/images/bonnell-headshot.jpg"}
             rounded
             fluid
           />
@@ -24,18 +24,16 @@ function Header() {
           <h1 className="header">Nice to meet you</h1>
           <p className="headertxt">My name is Sarah.</p>
           <p className="headertxt">
-            Lover of life, animals, and nature, I strive to enjoy every minute
-            on this planet.
+            Junior Web Developer with a background in commercial real estate and
+            marketing coupled with a life-long dedication to learning.
+            Certificate in Full Stack Web Development from University of Arizona
+            Coding Boot Camp with skills in HTML5, CSS, JavaScript, Wireframing,
+            and User Interface Development. Driven by collaborative and
+            independent efforts to combine creativity and problem solving to
+            develop user-friendly applications. An Economics and French language
+            background, strong research skills with an eye for detail, and big
+            picture vision make me a significant addition to any team. 
           </p>
-          <p className="headertxt">
-            Every opportunity presented provides a chance to learn something
-            new, and what's better than enriching your understanding of the
-            world around you!
-          </p>
-          <p className="headertxt">
-            Feel free to peruse; I welcome any feedback.
-          </p>
-
           <div>
             <a
               href="https://drive.google.com/file/d/1PdPPVWDQrDSOSrAmNP79qaqPq1I1TRTT/view?usp=sharing"
